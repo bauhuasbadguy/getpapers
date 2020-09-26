de84d906cf5ffb6c86145c65e721e3866629b156
Stuart <bauhaus.bad.guy@gmail.com>
2020-09-26 16:18:22 +0100

    uncommented run command

--- run_paper_get.sh
+++ run_paper_get.sh
@@ -2,7 +2,7 @@
 
 docker build -t paper_getter .
 
-#docker run -it --rm --name get-papers \
-# -v $(pwd)/xml_results:/xml_results \
-# paper_getter \
-# getpapers -p -x -o /xml_results --query 'chloroquine'
+docker run -it --rm --name get-papers \
+ -v $(pwd)/xml_results:/xml_results \
+ paper_getter \
+getpapers -p -x -o /xml_results --query 'chloroquine'
